de7525feaf37875da166cd662bfa26f3b644bcaf
Shota Kuroki <kurokis@users.noreply.github.com>
2017-08-31 16:14:41 +0900

    Added new marker map

--- MarkerServer/src/markerserver.cpp
+++ MarkerServer/src/markerserver.cpp
@@ -15,9 +15,10 @@ int main(int argc, char const *argv[])
   Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
   Camera.set(CV_CAP_PROP_FPS, 60);
 
-  aruco_wrapper System("../input_data/intrinsics2.yml", 
-                       "../input_data/map_2.yml");
-
+//  aruco_wrapper System("../input_data/intrinsics2.yml",
+//                       "../input_data/map_2.yml");
+  aruco_wrapper System("../input_data/intrinsics3.yml",
+                       "../input_data/map_3.yml");
   tcp_server s;
 
   // accept connection
@@ -28,10 +29,10 @@ int main(int argc, char const *argv[])
     cout << "Capture is opened" << endl;
     for(;;){
       Camera.grab();
-      Camera.retrieve(img);      
+      Camera.retrieve(img);
       if (System.MarkerUpdate(img)) {
         // display state when marker is detected
-        System.Disp();        
+        System.Disp();
       }
       System.Logging();
       s.send_data((const char *)System.Packet(), sizeof(*System.Packet()));
