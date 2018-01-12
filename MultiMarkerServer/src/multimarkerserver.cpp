2057fd2b31354fec14e925b26249ff16ccb932a8
akurosaki <kurosaki-akihiro@g.ecc.u-tokyo.ac.jp>
2018-01-12 19:50:57 +0900

    Added status check before sending. Updated droneport and payload yml.

--- MultiMarkerServer/src/multimarkerserver.cpp
+++ MultiMarkerServer/src/multimarkerserver.cpp
@@ -17,12 +17,12 @@ int main(int argc, char const *argv[])
   string camera_parameter_path =
     "../input_data/camera_module_v2_640x480.yml";
   std::vector<string> marker_map_paths = {
-    "../input_data/droneport_old.yml",
+    "../input_data/droneport.yml",
     "../input_data/payload.yml"};
   multi_aruco_wrapper System(camera_parameter_path,marker_map_paths);
   UTFIFOWriter fw_marker = UTFIFOWriter("/dev/marker_fifo");
   UTFIFOWriter fw_payload = UTFIFOWriter("/dev/payload_fifo");
-  
+
   if (Camera.open()) {
     cout << "Capture is opened" << endl;
     for(;;){
@@ -33,8 +33,12 @@ int main(int argc, char const *argv[])
         System.Disp();
       }
       System.Logging();
-      fw_marker.send_data((const char *)System.Packet(0), sizeof(*System.Packet(0)));
-      fw_payload.send_data((const char *)System.Packet(1), sizeof(*System.Packet(1)));
+      if(System.Packet(0)->status == 1){
+        fw_marker.send_data((const char *)System.Packet(0), sizeof(*System.Packet(0)));
+      }
+      if(System.Packet(1)->status == 1){
+        fw_payload.send_data((const char *)System.Packet(1), sizeof(*System.Packet(1)));
+      }
     }
   }
     return 0;
